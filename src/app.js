8c2df37e81f4c636b43bd92f21914a03a475aa05
Alankar <alankar010499@gmail.com>
2024-12-29 21:57:44 +0530

    add user route with multiple handlers in app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+app.get(
+  "/user",
+  (req, res, next) => {
+    console.log("Route handler 1 is called");
+    res.send("response");
+    next();
+  },
+  (req, res) => {
+    console.log("Route handler 2 is called");
+    res.send(" 2nd response");
+  }
+);
 
 app.listen(3000, () => {
   console.log("Server is started on port 3000");
